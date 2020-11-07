e7d3380d5cbe37a21442b7a660e3eed9cb7db1fb
sayhoChun <fishcreek@naver.com>
2020-11-08 02:03:57 +0900

    added ad to search.php

--- ajaxPages/ajaxSearchList.php
+++ ajaxPages/ajaxSearchList.php
@@ -3,9 +3,46 @@
 $router = new BoardRoute();
 //    $list = $router->getNoticeList();
 $list = $router->searchList();
+$idx = 0;
+$target = mt_rand(1, 5);
 ?>
 <?foreach($list as $item){
+        $idx++;
     ?>
+    <script src="assets/js/jquery.min.js"></script>
+    <script>
+        jQuery('img.svg').each(function(){
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+
+            jQuery.get(imgURL, function(data){
+                var $svg = jQuery(data).find('svg');
+                if(typeof imgID !== 'undefined') $svg = $svg.attr('id', imgID);
+                if(typeof imgClass !== 'undefined') $svg = $svg.attr('class', imgClass+' replaced-svg');
+                $svg = $svg.removeAttr('xmlns:a');
+                if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+                $img.replaceWith($svg);
+            }, 'xml');
+        });
+    </script>
+    <style>
+        svg {width: 2.0rem; height: 2.0rem;}
+        svg path {fill: #FFFFFF !important;}
+
+        .media{
+            margin-top: 0;
+            margin-bottom: 0 !important;
+        }
+        @media screen and (min-width: 770px) {
+            .media{
+                margin-top: 4.0rem;
+                margin-bottom: 2.0em !important;
+            }
+        }
+    </style>
+
     <div noticeID="<?=$item["id"]?>" class="col-twelve jDetail text-right"
          style="padding:0px 10px;font-size:13px;border: 1px #BBBBBB solid; border-radius:5px;margin-bottom: 10px;">
         <div class="text-left">
@@ -24,4 +61,25 @@ $list = $router->searchList();
             <p style="margin-bottom: 12px;"></p>
         </div>
     </div>
+    <?if($idx == $target){?>
+        <div class="col-twelve text-right" style="padding:0px 10px;font-size:13px;border: 1px #BBBBBB solid; border-radius:5px;margin-bottom: 10px;">
+            <div class="text-left">
+                <h5 style="margin-top: 12px; font-size: 15px; margin-bottom: 5px;">
+                    <i class="fa fa-dot-circle"></i>&nbsp; (광고) 충무로 광안리</h5>
+            </div>
+            <hr style="margin:0;" />
+            <div class="align-left">
+                <p>
+                    <span class="image left">
+                        <img src="images/ad.jpg" alt="" style=""/>
+                    </span>
+                    충무로 방어, 연어, 육사시미 맛집: 광안리
+                </p>
+                <p class="media" style="">서울 중구 충무로2길 32 제1호</p>
+                <div class="align-right">
+                    <img src="assets/css/images/ad.svg" class="svg" style="width: 2.0rem; color: white"/>
+                </div>
+            </div>
+        </div>
+    <?}?>
 <?}?>
